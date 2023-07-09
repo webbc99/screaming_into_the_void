6ace224afb7dcdf2b109b747f58367b2068e9ab1
Aerodyne <webbc99@gmail.com>
2023-07-09 14:45:46 +0100

    Added home controller and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
-  def latest
+  before_action :set_post, only: [:show]
+
+  def index
+    @posts = Post.order(created_at: :desc)
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.last
+  end
+
 end
